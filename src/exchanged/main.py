6ce546b89a6fad5bd5b7f19fa1962750fe62a911
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-06-28 20:14:16 +0900

    WIP

--- src/exchanged/main.py
+++ src/exchanged/main.py
@@ -1,5 +1,6 @@
 import torch
 import math
+import numpy as np
 from random import random
 
 class Network(torch.nn.Module):
@@ -27,36 +28,62 @@ class Network(torch.nn.Module):
         x = torch.nn.functional.log_softmax(x, dim=-1)
         return x
 
+def text_barchart(arr, low=0.0, high=1.0, rounding = round):
+    levels = "▁▂▃▄▅▆▇█"
+
+    def get_level(x):
+        t = (x - low) / (high - low)
+        if math.isnan(t):
+            t = 0
+        i = max(0, min(7, rounding(t * 8)))
+        return levels[i]
+
+    chars = [get_level(x) for x in arr]
+    return "".join(chars)
+
 
 def main():
-    model = Network()
-    model_2 = Network()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
-    optimizer_2 = torch.optim.AdamW(model_2.parameters())
-    model_state = model.state_dict()
-    model_2.load_state_dict(model_state)
-    sampled = []
-    log_prob_sequence: list[float] = []
-    probabilities = []
+    #model_2 = Network()
+    #optimizer_2 = torch.optim.AdamW(model_2.parameters())
+    #model_state = model.state_dict()
+    #model_2.load_state_dict(model_state)
     steps = 10000
-    for step in range(steps):
-        optimizer.zero_grad()
-        log_probs = model()
-        probs = torch.exp(log_probs)
-        r = random()
-        probabilities.append(probs[1].item())
-        if step % (steps // 100) == 0:
-            print(probs[1].item())
-        if r <= probs[0]:
-            loss = probs[1]
-            log_prob_sequence.append(log_probs[0])
-            sampled.append(0)
-        else:
-            loss = probs[0]
-            log_prob_sequence.append(log_probs[1])
-            sampled.append(1)
-        loss.backward()
-        optimizer.step()
-    log_probs = torch.tensor(log_prob_sequence)
-    print(torch.sum(log_probs))
+    runs = 1000
+    final = []
+    for run in range(runs):
+        model = Network()
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+        sampled = []
+        log_prob_sequence: list[float] = []
+        probabilities = []
+        for step in range(steps):
+            optimizer.zero_grad()
+            log_probs = model()
+            probs = torch.exp(log_probs)
+            probabilities.append(probs)
+            r = random()
+            # if step % (steps // 100) == 0:
+            #     print(f"{probs[0].item(): 3.3f} {probs[1].item(): 3.3f}")
+            if r <= probs[0]:
+                index = 0
+            else:
+                index = 1
+            # negative log-likelihood loss
+            loss = -log_probs[index]
+            log_prob_sequence.append(log_probs[index])
+            sampled.append(index)
+            loss.backward()
+            optimizer.step()
+        final.append(probabilities[-1][1].item())
+        hist, _ = np.histogram(final, range=(0,1), bins=20)
+        print(text_barchart(hist, high=np.amax(hist)), run)
+        # log_probs = torch.tensor(log_prob_sequence)
+        # for i, p in enumerate(torch.exp(log_probs).tolist()):
+        #     if i % 10 == 0:
+        #         end = '\n'
+        #     else:
+        #         end = ' '
+        #     print(f"{p:2.4f}", end=end)
+        # print()
+        # print(torch.sum(log_probs))
     # print(probabilities)
